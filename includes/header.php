22bbc7a6aab9e092cc9804f95572599a53e25e69
Janaki142 <123352388+Janaki142@users.noreply.github.com>
2023-02-10 16:07:39 +0530

    10febcommit
    10febcommit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">JANAKIRAM  Cinema</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="index.php" style="color:black">Home</a></li>
+            <li><a href="schedule.php" style="color:black">Schedule</a></li>
+            <!-- <li><a href="TxnStatus.php" target="_blank">Status</a></li> -->
+            <li><a href="#" style="color:black">Team</a></li>           
+            <li><a href="contact-us.php" style="color:black">Contact</a></li>
+        </ul>
+    </nav>
+</div>
